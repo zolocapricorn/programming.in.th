4480b850722b223e8b95d9a3b4f5846f67fca727
KamolchanokSinraksa <77113968+zolocapricorn@users.noreply.github.com>
2021-05-30 20:07:22 +0700

    Create 16. Ptice.py

---
+++ 16. Ptice.py
@@ -0,0 +1,23 @@
+def main(number, alphabet):
+    Adrian = ["A", "B", "C"]
+    Bruno = ["B", "A", "B", "C"]
+    Goran = ["C", "C", "A", "A", "B", "B"]
+    count_A, count_B, count_G = 0, 0, 0
+    for a in range(len(alphabet)):
+        if Adrian[a%3] == alphabet[a]:
+            count_A += 1
+        if Bruno[a%4] == alphabet[a]:
+            count_B += 1
+        if Goran[a%6] == alphabet[a]:
+            count_G += 1
+    if [count_A, count_B, count_G] == number:
+        print(number)
+    print(max(count_A, count_B, count_G))
+    if count_A == max(count_A, count_B, count_G):
+        print("Adrian")
+    if count_B == max(count_A, count_B, count_G):
+        print("Bruno")
+    if count_G == max(count_A, count_B, count_G):
+        print("Goran")
+    
+main(int(input()), input())
