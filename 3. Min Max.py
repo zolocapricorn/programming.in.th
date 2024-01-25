947cf904dd5c19fe7da8ef0880d9f02a0c6e8b84
Kamolchanok Sinraksa <77113968+zolocapricorn@users.noreply.github.com>
2024-01-25 23:43:40 +0700

    Update and rename 3. Min Max.py to Min Max.py
    Rename file and change code structure into function.

--- 3. Min Max.py
+++
@@ -1,4 +0,0 @@
-count, keep = int(input()), []
-for _ in range(count):
-    keep.append(int(input()))
-print(min(keep)), print(max(keep))
---
+++ Min Max.py
@@ -0,0 +1,7 @@
+def min_max(count):
+    keep = []
+    for _ in range(count):
+        keep.append(int(input()))
+    print(min(keep)), print(max(keep))
+min_max(int(input()))
+
