b35226ca743848a37e25a76df4a70ba35f4cc669
zolocapricorn <suijuku@outlook.com>
2024-01-26 00:33:59 +0700

    Update file.

--- Matrix_Addition.py
+++ Matrix_Addition.py
@@ -1,14 +1,13 @@
-def main(data, mt1, mt2, result):
+def main(data, result):
+    result = []
     rc = [int(a) for a in data]
-    for b in range(rc[0]):
-        mt1.append([int(c) for c in input().split()])
-    for d in range(rc[0]):
-        mt2.append([int(e) for e in input().split()])
+    mt1 = [[int(c) for c in input().split()] for b in range(rc[0])]
+    mt2 = [[int(e) for e in input().split()] for d in range(rc[0])]
     for f in range(rc[0]):
         for g in range(rc[1]):
             result.append(mt1[f][g]+mt2[f][g])
             if g == rc[1]-1:
                 print(*result)
                 result = []
-main(input().split(), [], [], [])
+main(input().split(), [])
 
