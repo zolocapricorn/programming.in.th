042eb95ce25fe219c129ef04cb85e5a4c8a1b1f6
zolocapricorn <suijuku@outlook.com>
2024-01-26 00:50:59 +0700

    Rename and edit code.

--- 8. x2.py
+++
@@ -1,5 +0,0 @@
-def main(value):
-    x1, s1 = int(value[0]), int(value[1])
-    x2 = (s1*2)-x1
-    print(x2)
-main(input().split())
---
+++ x2.py
@@ -0,0 +1,5 @@
+def x2(value):
+    x1, s1 = int(value[0]), int(value[1])
+    print((s1*2)-x1)
+x2(input().split())
+
