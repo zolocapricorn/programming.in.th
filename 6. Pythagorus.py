d02e6bec05a8efd07cbd6820fa9025a61c66af48
zolocapricorn <suijuku@outlook.com>
2024-01-26 00:43:40 +0700

    Edit and rename file.

--- 6. Pythagorus.py
+++
@@ -1,3 +0,0 @@
-from math import sqrt
-lenght = input().split(" ")
-print("{:.6f}".format(((float(lenght[0])))**2 + float(lenght[1])**2)**0.5)
---
+++ Pythagorus.py
@@ -0,0 +1,4 @@
+def pythagorus(lenght):
+    print("{:.6f}".format((float(lenght[0])**2 + float(lenght[1])**2)**0.5))
+pythagorus(input().split(" "))
+
