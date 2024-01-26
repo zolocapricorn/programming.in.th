c8b68f5aa41d666408197427fffca37188f75b0b
zolocapricorn <suijuku@outlook.com>
2024-01-26 13:13:13 +0700

    Edit code and rename file.

--- Perket.py
+++ Perket.py
@@ -1,12 +1,10 @@
 from itertools import combinations as cbnt
-def main(igd, data, result):
-    for a in range(igd):
-        change = [int(b) for b in input().split()]
-        data.append(change)
+def perket(igd):
+    result = []
+    data = [[int(b) for b in input().split()] for a in range(igd)]
     for c in range(1, igd+1):
         if c == 1:
-            for d in data:
-                result.append(abs(d[0]-d[1]))
+            result += [abs(d[0]-d[1]) for d in data]
         else:
             think = list(cbnt(data, c))
             for e in think:
@@ -16,4 +14,5 @@ def main(igd, data, result):
                     bt += e[f][1]
                 result.append(abs(sour-bt))
     print(min(result))
-main(int(input()), [], [])
+perket(int(input()))
+
