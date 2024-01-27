46aa541fc9b27afd1408f41799ee0ac1b0973cb9
zolocapricorn <suijuku@outlook.com>
2024-01-27 15:15:23 +0700

    Rename file and function.

--- Datum (library).py
+++ Datum (library).py
@@ -1,6 +1,9 @@
 import datetime as dt
-def main(data):
+
+
+def datum(data):
     dm = [int(a) for a in data]
     date = dt.datetime(2009, dm[1], dm[0])
     print(date.strftime("%A"))
-main(input().split())
+datum(input().split())
+
--- Datum (non-library).py
+++ Datum (non-library).py
@@ -1,6 +1,7 @@
-def main(dm):
+def datum(dm):
     day = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
     month = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
     change = [int(a) for a in dm]
     print(day[(change[0]+(month[change[1]-1]))%7])
-main(input().split())
+datum(input().split())
+
