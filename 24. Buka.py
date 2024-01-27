78e43ef763cd8998f5da55f960dd5a04f0e35b3f
zolocapricorn <suijuku@outlook.com>
2024-01-27 15:30:25 +0700

    Rename file and function name.

--- 24. Buka.py
+++ 24. Buka.py
@@ -1,18 +1,8 @@
-def main(A, opr, B):
-    if opr == "*":
-        point = (A.count("0"))+(B.count("0"))
-        print(1, end="")
-        for a in range(point):
-            print(0, end="")
+def buka(first, operand, second):
+    if operand == "+":
+        print(first+second)
     else:
-        if len(A) < len(B):
-            B = A
-        keep = [ b for b in A]
-        for c in range(len(B)):
-            keep.pop(-1)
-        for d in B:
-            keep.append(d)
-        print(*keep, sep="")
-        else:
+        print(first*second)
             
-main(input(), input(), input())
+buka(int(input()), input(), int(input()))
+
